aa2a702cad44c2fc5578e0c5e0ebfc8da0e3f58f
weusyeas2 <2026688007@ogr.cu.edu.tr>
2026-09-18 11:59:11 +0300

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -21,18 +21,17 @@ def hesap_makinesi():
      elif x=="4": 
           a=int (input("birinci sayiyi giriniz:"))
           b=int (input("ikinci sayiyi giriniz:"))
-          print("Sonuç:",a/b)
-          
           if b==0:
-              print("Hata! Bir sayi 0 ile bölünmez")
-          
+           print("Hata! Bir sayi 0 ile bölünmez")
+          else:
+           print("Sonuç:",a/b)
+            
      elif x=="5":
          print("cikis yapiliyor...")
          break
  
      else: print("hatali sayi girdiniz")
   
-
 hesap_makinesi()
 
      
