55c31a3813e56c4b9a43e2cd0cc452ef3c68f398
weusyeas2 <2026688007@ogr.cu.edu.tr>
2026-09-18 12:06:22 +0300

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -1,8 +1,7 @@
 #küçük hesap makinesi
 def hesap_makinesi():
  while True:
-
-     x= input ("1.topla 2.cikar 3.carp 4 .bol 5.cikis işlemlerinden birini seçiniz:") 
+     x= input ("1.topla 2.cikar 3.carp 4.bol 5.cikis işlemlerinden birini seçiniz:") 
      if x=="1":
           a=int (input("birinci sayiyi giriniz:"))
           b=int (input("ikinci sayiyi giriniz:"))
@@ -21,24 +20,14 @@ def hesap_makinesi():
      elif x=="4": 
           a=int (input("birinci sayiyi giriniz:"))
           b=int (input("ikinci sayiyi giriniz:"))
-
           if b==0:
            print("Hata! Bir sayi 0 ile bölünmez")
           else:
            print("Sonuç:",a/b)
-            
 
-     if b==0:
-              print("Hata! Bir sayi 0 ile bölünmez")
-     else:
-          print("Sonuç:",a/b)
-      
      elif x=="5":
-     print("cikis yapiliyor...")
-     break
- 
- else: print("hatali sayi girdiniz")
-  
+      print("cikis yapiliyor...")
+      break
 hesap_makinesi()
 
      
